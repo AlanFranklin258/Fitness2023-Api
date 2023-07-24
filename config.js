06b6824e80cb25dcd321e476e86e959479f8a73c
AlanFranklin258 <58321608+AlanFranklin258@users.noreply.github.com>
2023-07-24 12:03:52 +0800

    Update config.js

--- config.js
+++ config.js
@@ -2,7 +2,7 @@ module.exports = {
     //jwt密钥
     secret: 'fintness-jwt-secret',
     // MongoDB 连接地址
-    db_url: 'mongodb://Alan:Franklin@60.205.181.92:27017/fitness2023?authSource=admin'
+    db_url: 'mongodb_address'
     // MongoDB 本地地址
     // db_url: 'mongodb://localhost:27017/fitness2023'
 }
